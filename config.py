0e771181a7ff858ae528d3013679905796406727
JeopardiseYouth <86751601+JeopardiseYouth@users.noreply.github.com>
2023-08-29 11:44:15 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-1.caavkgwerp9f.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lowjunyan-employee"
+customregion = "us-east-1"
+
