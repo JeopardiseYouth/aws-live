cfcd2e1aa460e41d6ce851533fe28ca755e030c7
LowJunYan <86751601+JeopardiseYouth@users.noreply.github.com>
2023-09-21 16:19:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-customhost = "database-1.caavkgwerp9f.us-east-1.rds.amazonaws.com"
+customhost = "employee.caavkgwerp9f.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowjunyan-employee"
+custombucket = "lowjunyan-lecturer"
 customregion = "us-east-1"
 
